4646828433be5f79998bb838e56cb894b71fc478
Battseren Badral <bbattseren88@gmail.com>
2025-03-28 14:10:12 +0100

    fix

--- train.py
+++ train.py
@@ -2,14 +2,13 @@ import os
 
 from trainer import Trainer, TrainerArgs
 
-from TTS.tts.configs.glow_tts_config import GlowTTSConfig
+from TTS.tts.configs.tacotron2_config import Tacotron2Config
 from TTS.tts.configs.shared_configs import BaseDatasetConfig, CharactersConfig
 from TTS.tts.datasets import load_tts_samples
-from TTS.tts.models.glow_tts import GlowTTS
+from TTS.tts.models.tacotron2 import Tacotron2
 from TTS.tts.utils.text.tokenizer import TTSTokenizer
 from TTS.utils.audio import AudioProcessor
 from utils.clean_cache import clean_gpu_cache
-from utils.get_batch_size import get_auto_batch_size, get_auto_num_workers
 from utils.get_latest_run import get_latest_checkpoint, get_latest_model
 import argparse
 
@@ -19,24 +18,18 @@ def train(dataset_path: str, output_path: str, restore_path: str, best_path: str
         meta_file_train="metadata_train.csv", 
         meta_file_val="metadata_test.csv",
         path=dataset_path,
-        language="mn-mn"
+        language="mn-cyrl",     
     )
     
-    config = GlowTTSConfig(
-        batch_size=get_auto_batch_size(is_training=True),
-        eval_batch_size=get_auto_batch_size(is_training=False),
-        num_loader_workers=get_auto_num_workers(is_training=True),
-        num_eval_loader_workers=get_auto_num_workers(is_training=False),
+    config = Tacotron2Config(
+        batch_size=64,
+        eval_batch_size=32,
+        num_loader_workers=4,
+        num_eval_loader_workers=4,
         run_eval=True,
-        test_delay_epochs=-1,
+        test_delay_epochs=5,
         epochs=1000,
         text_cleaner="basic_cleaners",
-        use_phonemes=False,
-        phoneme_language="mn-cyrl", 
-        phoneme_cache_path=os.path.join(output_path, "phoneme_cache"),
-        print_step=25,
-        print_eval=False,
-        mixed_precision=True,
         output_path=output_path,
         datasets=[dataset_config],
         characters=CharactersConfig(
@@ -61,7 +54,7 @@ def train(dataset_path: str, output_path: str, restore_path: str, best_path: str
         eval_split_size=config.eval_split_size,
     )
     
-    model = GlowTTS(
+    model = Tacotron2(
         config, 
         ap, 
         tokenizer, 
@@ -71,7 +64,7 @@ def train(dataset_path: str, output_path: str, restore_path: str, best_path: str
     trainer = Trainer(
         TrainerArgs(
             restore_path=restore_path,
-            best_path=best_path
+            best_path=best_path,
         ), 
         config, 
         output_path, 
